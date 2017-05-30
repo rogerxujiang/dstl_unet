0ed9e826282ad76f4167eefb2e2b08e2f2ac0afb
rogerjiang <jiangxu87@gmail.com>
2017-05-30 10:00:53 -0400

    update data_utils

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -193,6 +193,13 @@ def genetate_contours(polygonlist, img_size, xymax):
     if polygonlist is None:
         return None
 
+    to_ind = lambda x: np.array(list(x)).astype(np.float32)
+
+    perim_list = [convert_coordinate_to_raster(to_ind(poly.coords), img_size, xymax) for poly in polygonlist]
+
+    inter_list = [convert_coordinate_to_raster(to_ind(poly.coords), img_size, xymax)
+                  for poly_ex in polygonlist for poly in poly_ex.interiors]
+    '''
     for i in range(len(polygonlist)):
         poly = polygonlist[i]
         perim = np.array(list(poly.exterior.coords))
@@ -203,6 +210,8 @@ def genetate_contours(polygonlist, img_size, xymax):
             interior = np.array(list(pi.coords))
             interior_raster = convert_coordinate_to_raster(interior, img_size, xymax)
             inter_list.append(interior_raster)
+    '''
+
     return perim_list, inter_list
 
 
@@ -452,6 +461,7 @@ class ImageData():
         self._xymax = None
         self.label = None
         self.crop_image = None
+        self.CCCI = None
 
 
     def load_image(self):
@@ -545,6 +555,19 @@ class ImageData():
         self.label = labels
 
 
+    def create_CCCI(self):
+        if self.three_band_image is None:
+            self.load_image()
+        m = self.sixteen_band_image[:, :, 8:]
+        rgb = self.three_band_image
+        RE = resize(m[5, :, :], (rgb.shape[0], rgb.shape[1]))
+        MIR = resize(m[7, :, :], (rgb.shape[0], rgb.shape[1]))
+        R = rgb[:, :, 0]
+        # canopy chloropyll content index
+        # binary = (CCCI > 0.11).astype(np.float32) marks water fairly well
+        self.CCCI = (MIR - RE) / (MIR + RE) * (MIR - R) / (MIR + R)
+
+
     def visualize_images(self, plot_all=True):
 
         if self.label is None:
@@ -630,20 +653,9 @@ Some nice code:
 
 https://www.kaggle.com/drn01z3/end-to-end-baseline-with-u-net-keras
 
-def mask_for_polygons(polygons, im_size):
-    # __author__ = Konstantin Lopuhin
-    # https://www.kaggle.com/lopuhin/dstl-satellite-imagery-feature-detection/full-pipeline-demo-poly-pixels-ml-poly
-    img_mask = np.zeros(im_size, np.uint8)
-    if not polygons:
-        return img_mask
-    int_coords = lambda x: np.array(x).round().astype(np.int32)
-    exteriors = [int_coords(poly.exterior.coords) for poly in polygons]
-    interiors = [int_coords(pi.coords) for poly in polygons
-                 for pi in poly.interiors]
-    cv2.fillPoly(img_mask, exteriors, 1)
-    cv2.fillPoly(img_mask, interiors, 0)
-    return img_mask
-    
+from shapely.geometry import MultiPolygon, Polygon
+from collections import defaultdict
+
 def mask_to_polygons(mask, epsilon=5, min_area=1.):
     # __author__ = Konstantin Lopuhin
     # https://www.kaggle.com/lopuhin/dstl-satellite-imagery-feature-detection/full-pipeline-demo-poly-pixels-ml-poly
@@ -721,60 +733,8 @@ print('Final jaccard',
 
 
 
-from shapely.geometry import MultiPolygon, Polygon
-from collections import defaultdict
-def mask_to_polygons(mask, epsilon=10., min_area=10.):
-    # first, find contours with cv2: it's much faster than shapely
-    image, contours, hierarchy = cv2.findContours(
-        ((mask == 1) * 255).astype(np.uint8),
-        cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
-    # create approximate contours to have reasonable submission size
-    approx_contours = [cv2.approxPolyDP(cnt, epsilon, True)
-                       for cnt in contours]
-    if not contours:
-        return MultiPolygon()
-    # now messy stuff to associate parent and child contours
-    cnt_children = defaultdict(list)
-    child_contours = set()
-    assert hierarchy.shape[0] == 1
-    # http://docs.opencv.org/3.1.0/d9/d8b/tutorial_py_contours_hierarchy.html
-    for idx, (_, _, _, parent_idx) in enumerate(hierarchy[0]):
-        if parent_idx != -1:
-            child_contours.add(idx)
-            cnt_children[parent_idx].append(approx_contours[idx])
-    # create actual polygons filtering by area (removes artifacts)
-    all_polygons = []
-    for idx, cnt in enumerate(approx_contours):
-        if idx not in child_contours and cv2.contourArea(cnt) >= min_area:
-            assert cnt.shape[1] == 1
-            poly = Polygon(
-                shell=cnt[:, 0, :],
-                holes=[c[:, 0, :] for c in cnt_children.get(idx, [])
-                       if cv2.contourArea(c) >= min_area])
-            all_polygons.append(poly)
-    # approximating polygons might have created invalid ones, fix them
-    all_polygons = MultiPolygon(all_polygons)
-    if not all_polygons.is_valid:
-        all_polygons = all_polygons.buffer(0)
-        # Sometimes buffer() converts a simple Multipolygon to just a Polygon,
-        # need to keep it a Multi throughout
-        if all_polygons.type == 'Polygon':
-            all_polygons = MultiPolygon([all_polygons])
-    return all_polygons
 
 
-'''
-
-'''
-https://www.kaggle.com/resolut/waterway-0-095-lb
-this  index can be used to classify water
-def CCCI_index(m, rgb):
-    RE  = resize(m[5,:,:], (rgb.shape[0], rgb.shape[1]))
-    MIR = resize(m[7,:,:], (rgb.shape[0], rgb.shape[1]))
-    R = rgb[:,:,0]
-    # canopy chloropyll content index
-    CCCI = (MIR-RE)/(MIR+RE)*(MIR-R)/(MIR+R)
-    return CCCI
-# The following is almost enough for classifying water
-binary = (CCCI > 0.11).astype(np.float32)
+https://www.kaggle.com/aamaia/large-vehicles
+crop out all vehicles
 '''
